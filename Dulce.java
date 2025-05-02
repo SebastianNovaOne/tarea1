e14b196d660e54fb4ae930ac0e8ffcf642c0010a
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 01:28:02 -0400

    Se añade documentacion Javadoc

--- Dulce.java
+++ Dulce.java
@@ -1,13 +1,27 @@
+/**
+ * Clase que representa un dulce.
+ * Hereda de la clase producto.
+ */
 public class Dulce extends Producto {
     private String nombre;
 
-    public Dulce(String nombre, int precio){
+    /**
+     * Constructor de la clase dulce.
+     *
+     * @param nombre Nombre del dulce.
+     * @param precio Precio del dulce.
+     */
+    public Dulce(String nombre, int precio) {
         super(precio);
         this.nombre = nombre;
     }
 
+    /**
+     * Retorna el nombre del dulce.
+     *
+     * @return Nombre del producto.
+     */
     public String getNombre() {
         return nombre;
     }
-
 }
