4391b7fea19611d9f5fcc8f03f1d5a5eba001612
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 18:06:18 -0400

    Se crea constructor qur crea un arreglo de depositos, uno para cada producto definido en ProductoEnum.  Tambien llena cada depósito con la cantidad inicial de productos usando el método crearProducto() de hare ahora.

--- Expendedor.java
+++ Expendedor.java
@@ -1,4 +1,14 @@
 public class Expendedor {
     private final Deposito<Producto>[] depositos;
     private final Deposito<Moneda> depositoVuelto = new Deposito<>();
+
+    public Expendedor(int cantidadInicial) {
+        depositos = new Deposito[ProductoEnum.values().length];
+        for (int i = 0; i < ProductoEnum.values().length; i++) {
+            depositos[i] = new Deposito<>();
+            for (int j = 0; j < cantidadInicial; j++) {
+                depositos[i].agregar(crearProducto(ProductoEnum.values()[i]));
+            }
+        }
+    }
 }
