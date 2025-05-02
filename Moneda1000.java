17405415953305ea044e30de0b6e73b6c95b1f74
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-01 23:49:20 -0400

    Se crea clase Moneda1000 que hereda de Moneda con metodo getValor
    Se añade documentacion Javadoc

---
+++ Moneda1000.java
@@ -0,0 +1,16 @@
+/**
+ * Clase que representa una moneda de 1000 pesos.
+ * Hereda de la clase Moneda.
+ */
+public class Moneda1000 extends Moneda {
+
+    /**
+     * Retorna el valor de la moneda, que es 1000 pesos.
+     *
+     * @return Valor de la moneda (1000).
+     */
+    @Override
+    public int getValor() {
+        return 1000;
+    }
+}
