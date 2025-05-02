f84cf323b86ab38d6c029f62597c9f9a3c56abf7
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-01 23:36:53 -0400

    Se crea clase Moneda100 que hereda de Moneda con metodo getValor
    Se añade documentacion Javadoc

---
+++ Moneda100.java
@@ -0,0 +1,16 @@
+/**
+ * Clase que representa una moneda de 100 unidades.
+ * Hereda de la clase Moneda.
+ */
+public class Moneda100 extends Moneda {
+
+    /**
+     * Retorna el valor de la moneda, que es 100.
+     *
+     * @return Valor de la moneda (100).
+     */
+    @Override
+    public int getValor() {
+        return 100;
+    }
+}
