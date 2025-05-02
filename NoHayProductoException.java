8f89b980ad3bdd80c87fa862b382f0e7d6b9f817
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 02:32:25 -0400

    Se añade documentacion Javadoc

--- NoHayProductoException.java
+++ NoHayProductoException.java
@@ -1,5 +1,13 @@
+/**
+ * Excepcion que ocurre cuando no queda del producto en el expendedor.
+ */
 public class NoHayProductoException extends Exception {
 
+    /**
+     * Constructor de la excepcion.
+     *
+     * @param mensaje Mensaje que describe el error.
+     */
     public NoHayProductoException(String mensaje) {
         super(mensaje);
     }
