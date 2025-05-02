f19290c61c2dae01aa815e1cffa6d1bc18a7db0b
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 02:46:06 -0400

    Se añade documentacion Javadoc

--- PagoInsuficienteException.java
+++ PagoInsuficienteException.java
@@ -1,4 +1,14 @@
+/**
+ * Excepcion que aparece cuando el pago realizado es insuficiente.
+ * Si el valor de la moneda es menor al valor del producto ocurre este error.
+ */
 public class PagoInsuficienteException extends Exception {
+
+    /**
+     * Constructor de la excepcion.
+     *
+     * @param mensaje Mensaje que describe el error.
+     */
     public PagoInsuficienteException(String mensaje) {
         super(mensaje);
     }
