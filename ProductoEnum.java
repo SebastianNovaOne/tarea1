ba4d43bafccbd521903b1d68b286ba9588c1180e
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 01:55:43 -0400

    Se añade documentacion Javadoc

--- ProductoEnum.java
+++ ProductoEnum.java
@@ -1,3 +1,7 @@
+/**
+ * Enumeracion de los dulces y bebidas disponibles.
+ * Cada producto tiene un precio asociado.
+ */
 public enum ProductoEnum {
     COCACOLA(900),
     SPRITE(900),
@@ -7,10 +11,20 @@ public enum ProductoEnum {
 
     private final int precio;
 
+    /**
+     * Constructor de la enumeracion.
+     *
+     * @param precio Precio asociado al producto.
+     */
     ProductoEnum(int precio) {
         this.precio = precio;
     }
 
+    /**
+     * Retorna el precio del producto.
+     *
+     * @return Precio.
+     */
     public int getPrecio() {
         return precio;
     }
