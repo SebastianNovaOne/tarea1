cdf40c28db2d39224adb22a808c4412abed07499
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 01:40:19 -0400

    Se crea metodo getPrecio() que obtiene el precio. Se corrige error de typo (de , a ; en SUPER8(900);)

--- ProductoEnum.java
+++ ProductoEnum.java
@@ -3,11 +3,15 @@ public enum ProductoEnum {
     SPRITE(900),
     FANTA(900),
     SNICKERS(400),
-    SUPER8(100),
+    SUPER8(100);
 
     private final int precio;
 
     ProductoEnum(int precio) {
         this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return precio;
     }
 }
