6e686609e96d5576a54ce0b58a0d52d27d8afff0
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 02:22:08 -0400

    Se añade documentacion Javadoc

--- PagoIncorrectoException.java
+++ PagoIncorrectoException.java
@@ -1,4 +1,14 @@
+/**
+ * Excepcion que se lanza cuando el pago realizado es incorrecto.
+ * Por ejemplo, si se intenta usar una moneda de 200 pesos.
+ */
 public class PagoIncorrectoException extends Exception {
+
+    /**
+     * Constructor de la excepcion.
+     *
+     * @param mensaje Mensaje que describe el error.
+     */
     public PagoIncorrectoException(String mensaje) {
         super(mensaje);
     }
