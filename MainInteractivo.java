b04ee28cbf13c352179c59c8d2468436ebdb7e27
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 21:12:11 -0400

    Se crea clase principal MainInteractivo. Se crea un Scanner para leer entradas del usuario. Luego se crea un Expendedor con 10 unidades de cada producto.

---
+++ MainInteractivo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class MainInteractivo {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Expendedor expendedor = new Expendedor(10);
+    }
+}
