cc6466d77b7ad3dff4b5370c007f1cf9bf0e36e5
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 21:25:14 -0400

    Se añade un while true que muestra un menu con opciones de productos para que el usuario ingrese un número entre 1 y 6 (cada numero un dulce o bebida). Se añade if para que si se elige 6 (salir), se sale del loop y se finaliza el programa

--- MainInteractivo.java
+++ MainInteractivo.java
@@ -5,5 +5,22 @@ public class MainInteractivo {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         Expendedor expendedor = new Expendedor(10);
+
+        while (true) {
+            System.out.println("\nMenu de Expendedor");
+            System.out.println("1. Coca Cola");
+            System.out.println("2. Sprite");
+            System.out.println("3. Fanta");
+            System.out.println("4. Snickers");
+            System.out.println("5. Super8");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione Producto (1-6): ");
+            int opcion = scanner.nextInt();
+
+            if (opcion == 6) {
+                System.out.println("Saliendo");
+                break;
+            }
+        }
     }
 }
