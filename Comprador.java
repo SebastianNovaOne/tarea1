faeb530beb383e104891880e86a7628a17e8f76e
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 21:06:51 -0400

    Se añade documentacion javadoc.

--- Comprador.java
+++ Comprador.java
@@ -1,7 +1,21 @@
+/**
+ * Clase que representa a un comprador que adquiere un producto desde un expendedor.
+ * Registra el nombre del producto consumido y el total del vuelto recibido.
+ */
 public class Comprador {
     private int totalVuelto;
     private String productoConsumido;
 
+    /**
+     * Constructor que realiza la compra de un producto desde un expendedor.
+     *
+     * @param m             Moneda utilizada para realizar la compra.
+     * @param tipoProducto  Indice del producto segun el orden en ProductoEnum.
+     * @param expendedor    Expendedor desde el cual se realiza la compra.
+     * @throws PagoIncorrectoException   Si la moneda es nula.
+     * @throws NoHayProductoException    Si el producto no esta disponible.
+     * @throws PagoInsuficienteException Si la moneda no cubre el precio del producto.
+     */
     public Comprador(Moneda m, int tipoProducto, Expendedor expendedor)
             throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
         Producto producto = expendedor.comprarProducto(m, ProductoEnum.values()[tipoProducto]);
@@ -10,15 +24,25 @@ public class Comprador {
             totalVuelto = 0;
             Moneda monedaRecibida;
             while ((monedaRecibida = expendedor.getVuelto()) != null) {
-                totalVuelto = totalVuelto + monedaRecibida.getValor();
+                totalVuelto += monedaRecibida.getValor();
             }
         }
     }
 
+    /**
+     * Retorna el nombre del producto consumido.
+     *
+     * @return Nombre del producto o null si no se consumo ninguno.
+     */
     public String getProductoConsumido() {
         return productoConsumido;
     }
 
+    /**
+     * Retorna el total del vuelto recibido en la compra.
+     *
+     * @return Monto total del vuelto.
+     */
     public int getVueltoTotal() {
         return totalVuelto;
     }
