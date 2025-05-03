d169eecc91a19c7d9978a9749db2dd99600af888
SebastianNovaOne <sebaxnovax@gmail.com>
2025-05-02 20:50:57 -0400

    Este constructor realiza la compra de un producto usando el expendedor y guarda su nombre. Si la compra fue exitosa, suma el total del vuelto recibido, moneda por moneda.

--- Comprador.java
+++ Comprador.java
@@ -1,4 +1,17 @@
 public class Comprador {
     private int totalVuelto;
     private String productoConsumido;
+
+    public Comprador(Moneda m, int tipoProducto, Expendedor expendedor)
+            throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
+        Producto producto = expendedor.comprarProducto(m, ProductoEnum.values()[tipoProducto]);
+        if (producto != null) {
+            productoConsumido = producto.getNombre();
+            totalVuelto = 0;
+            Moneda monedaRecibida;
+            while ((monedaRecibida = expendedor.getVuelto()) != null) {
+                totalVuelto = totalVuelto + monedaRecibida.getValor();
+            }
+        }
+    }
 }
